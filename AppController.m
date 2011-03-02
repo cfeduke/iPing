b3b38642c4fb9c2e82f8a3c18b78da59f63b47af
Charles Feduke <charles.feduke@gmail.com>
2011-03-02 07:28:26 -0500

    complete

--- AppController.m
+++ AppController.m
@@ -12,6 +12,56 @@
 @implementation AppController
 
 -(IBAction)startStopPing:(id)sender {
+	if (task) {
+		[task interrupt];
+	} else {
+		task = [[NSTask alloc] init];
+		[task setLaunchPath:@"/sbin/ping"];
+		NSArray *args = [NSArray arrayWithObjects:@"-c10", [hostField stringValue], nil];
+		[task setArguments:args];
+		
+		[pipe release];
+		pipe = [[NSPipe alloc] init];
+		[task setStandardOutput:pipe];
+		
+		NSFileHandle *fh = [pipe fileHandleForReading];
+		NSNotificationCenter *nc;
+		nc = [NSNotificationCenter defaultCenter];
+		[nc removeObserver:self];
+		[nc addObserver:self selector:@selector(dataReady:) name:NSFileHandleReadCompletionNotification object:fh];
+		[nc addObserver:self selector:@selector(taskTerminated:) name:NSTaskDidTerminateNotification object:task];
+		[task launch];
+		[outputView setString:@""];
+		
+		[fh readInBackgroundAndNotify];
+	}
+}
+
+-(void)appendData:(NSData *)d
+{
+	NSString *s = [[NSString alloc] initWithData:d encoding:NSUTF8StringEncoding];
+	NSTextStorage *ts = [outputView textStorage];
+	[ts replaceCharactersInRange:NSMakeRange([ts length], 0) withString:s];
+	[s release];
+}
+
+-(void)dataReady:(NSNotification *)n {
+	NSData *d;
+	d = [[n userInfo] valueForKey:NSFileHandleNotificationDataItem];
+	NSLog(@"dataReady: %d bytes", [d length]);
 	
+	if ([d length]) {
+		[self appendData:d];
+	}
+	
+	if (task)
+		[[pipe fileHandleForReading] readInBackgroundAndNotify];
+}
+
+-(void)taskTerminated:(NSNotification *)n {
+	NSLog(@"Task terminated");
+	[task release];
+	task = nil;
+	[startButton setState:0];
 }
 @end
